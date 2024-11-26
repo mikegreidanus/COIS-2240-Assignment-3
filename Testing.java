9c1096eb79459dc5678f8c84fd816683c4d6d953
mikegreidanus <mikegreidanus13@gmail.com>
2024-11-25 19:10:24 -0500

    Borrow/Return Validation Testing
    Created JUnit testing class. Created testBorrowReturn Method.

---
+++ Testing.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testing {
+
+	@Test
+		public void testBorrowReturn() throws Exception{
+			Book testbook1 = new Book(123, "Test1");
+			Member testmember1 = new Member(1111, "George");
+			//Test that borrowing a book causes it to be unavailable
+			Transaction.getTransaction().borrowBook(testbook1, testmember1);
+			assertFalse(testbook1.isAvailable());
+			
+			//Test that you can no longer borrow an unavailable book
+			assertFalse(Transaction.getTransaction().borrowBook(testbook1, testmember1));
+			
+			//Test that you can return the book
+			assertTrue(Transaction.getTransaction().returnBook(testbook1, testmember1));
+			
+			//Test returning fails when its not borrowed 
+			assertFalse(Transaction.getTransaction().returnBook(testbook1, testmember1));
+
+		
+	}
+
+}
