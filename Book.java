71def64f647357054a6daa3f7c3acf5455a0d243
mikegreidanus <mikegreidanus13@gmail.com>
2024-11-25 19:05:10 -0500

    Final Git Commit of all classes (In the correct File!)
    Note for the Marker: My git repositories got screwed up do to complications with the JUnit testing class, which originally wouldn't run in the workspace. So I have now switched to a correct workspace, where things work, but I need to copy all my previous work. Taher is aware of my unique circumstances. Sorry for the inconvience.

---
+++ Book.java
@@ -0,0 +1,46 @@
+public class Book {
+    private int id;
+    private String title;
+    private boolean available;
+
+    public Book(int id, String title) throws Exception{
+    	if (isValidId(id)){
+        	this.id = id;
+        	this.title = title;
+        	this.available = true;
+    	}
+    	else {
+    		throw new Exception("Invalid Book Id");
+    	}
+    }
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    // Method to borrow the book
+    public void borrowBook() {
+        if (available) {
+            available = false;
+        }
+    }
+
+    // Method to return the book
+    public void returnBook() {
+        available = true;
+    }
+
+    // Method to check if a book id is valid
+    public boolean isValidId(int id) {
+        return id >= 100 && id <= 999;
+    }
+}
